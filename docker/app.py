f36fec95ff7505fd50bc79ffbdb29634a7b5bffd
kursova <gizemkursova@gmail.com>
2025-05-27 21:33:11 +0300

    added

--- docker/app.py
+++ docker/app.py
@@ -2,12 +2,12 @@ from flask import Flask
 import redis
 
 app = Flask(__name__)
-r = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    count = r.incr('hits')
-    return f'Bu sayfa {count} kez görüntülendi.'
+    count = cache.incr('hits')
+    return f'Hello! This page has been visited {count} times.'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
